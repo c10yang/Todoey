cbebe69e4d4b3bb58b8a093d4e15079d98493d16
Chen Yang <xiaotooo@hotmail.com>
2018-11-29 20:39:34 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by YC on 2018-11-28.
+//  Copyright © 2018 Chenditional Studio. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
