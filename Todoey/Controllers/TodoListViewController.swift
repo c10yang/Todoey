57061e849e868be44dd0cb3ad468aeee07ff7f3f
Chen Yang <xiaotooo@hotmail.com>
2018-11-30 20:24:47 -0500

    Persisting data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,33 +12,22 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
-    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem1 = Item ()
-        newItem1.title = "Find Mike"
-        itemArray.append(newItem1)
-        
-        let newItem2 = Item ()
-        newItem2.title = "Buy Eggs"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item ()
-        newItem3.title = "Destory Demogorgon"
-        itemArray.append(newItem3)
-        
-        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items
-        }
+        loadItems()
     }
     
     //MARK - Tableview Datasource Methods
+    
+    // Determine how many cells to dsplay
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    // Determin what to display on each cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
@@ -47,6 +36,7 @@ class TodoListViewController: UITableViewController {
         
         cell.textLabel?.text = item.title
         
+        // Display cell condition based on its done condition
         // value = condition ? valueIfTrue : valueIfFalse
         cell.accessoryType = item.done ? .checkmark : .none
         
@@ -54,10 +44,13 @@ class TodoListViewController: UITableViewController {
     }
     
     //Mark - TableView Delegate Methos
+    // When user select a cell what will happen
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-
+        
+        saveItems()
+        
         tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
@@ -82,9 +75,9 @@ class TodoListViewController: UITableViewController {
             
             self.itemArray.append(newItem)
             
-                self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            self.saveItems()
             
-                self.tableView.reloadData()
+            self.tableView.reloadData()
         }
         
         // Create a textfield for the alert window
@@ -98,6 +91,29 @@ class TodoListViewController: UITableViewController {
         
         // Display the alert
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        //Encoder
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array, \(error)")
+            }
+        }
     }
 }
 
